82456ae31faab6dc48fffdaa12a5e9df2386ab68
vclerc <valentin.clerc@product-live.com>
2020-09-29 17:40:11 +0200

    fix: Update console wrapper

--- packages/console-wrapper/src/console.wrapper.ts
+++ packages/console-wrapper/src/console.wrapper.ts
@@ -1,5 +1,5 @@
 export class ConsoleWrapper {
     public print(content: string) {
-        console.log('\x1b[36m%s\x1b[0m', `Printing: ${content}`);
+        console.log('\x1b[36m%s\x1b[0m', `Printing: ${content}-here`);
     }
 }
