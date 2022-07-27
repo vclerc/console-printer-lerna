25bce25fdb15fac16fa13cdd00c35ad70ff90710
vclerc <valentin.clerc@product-live.com>
2022-07-27 15:59:24 +0200

    fix: update messages

--- packages/test-server-alt/src/app/app.service.ts
+++ packages/test-server-alt/src/app/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
     private _consoleUtility: ConsoleUtility = new ConsoleUtility('alt');
 
     print() {
-        this._consoleUtility.print('test-server - 2-alt');
+        this._consoleUtility.print('test-server - alt');
     }
 
 }
