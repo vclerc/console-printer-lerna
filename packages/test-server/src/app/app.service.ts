8d8fb9be02c1b693ff365624cd1eff1a8d3f1889
vclerc <valentin.clerc@product-live.com>
2022-08-11 09:53:27 +0200

    fix: update test server text

--- packages/test-server/src/app/app.service.ts
+++ packages/test-server/src/app/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
     private _consoleUtility: ConsoleUtility = new ConsoleUtility('main');
 
     print() {
-        this._consoleUtility.print('test-server - main server - nope');
+        this._consoleUtility.print('test-server - main server - test');
     }
 
 }
