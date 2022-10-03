0b66b84baba611c7c08ae987c2638c852f497b47
vclerc <valentin.clerc@product-live.com>
2022-10-03 15:29:27 +0200

    fix: update mains erver response

--- packages/test-server/src/app/app.service.ts
+++ packages/test-server/src/app/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
     private _consoleUtility: ConsoleUtility = new ConsoleUtility('main');
 
     print() {
-        this._consoleUtility.print('test-server - main server - test1');
+        this._consoleUtility.print('test-server - main server - test2');
     }
 
 }
