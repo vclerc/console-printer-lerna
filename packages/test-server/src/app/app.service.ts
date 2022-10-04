fad6b476c59c53b30a0462d88e4d0e5882aa877f
vclerc <valentin.clerc@product-live.com>
2022-10-04 15:25:22 +0200

    fix: update server

--- packages/test-server/src/app/app.service.ts
+++ packages/test-server/src/app/app.service.ts
@@ -7,7 +7,7 @@ export class AppService {
     private _consoleUtility: ConsoleUtility = new ConsoleUtility('main');
 
     print() {
-        this._consoleUtility.print('test-server - main server - test6');
+        this._consoleUtility.print('test-server - main server - test7');
     }
 
 }
