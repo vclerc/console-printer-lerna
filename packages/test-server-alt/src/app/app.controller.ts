0bf134ca80317f976d7d1961393ad8e728336a55
vclerc <valentin.clerc@product-live.com>
2023-01-12 10:20:24 +0100

    feat: update alt server
    Release-As: 1.0.19-rc.1

--- packages/test-server-alt/src/app/app.controller.ts
+++ packages/test-server-alt/src/app/app.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller('app')
+@Controller('apps')
 export class AppController {
 
     constructor(
